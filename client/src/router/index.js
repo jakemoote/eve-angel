b8c19571f6a029e37d076547afe1fed4c8c82da6
Jake Moote <jakemoote@gmail.com>
2021-12-27 19:59:20 -0500

    Add auth check to front end Vue routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Assets from '../components/Assets.vue'
+import axios from "axios";
 
 const routes = [
     {
         path: '/',
         name: 'Login',
         component: Login,
+        meta: {
+            requires_guest: true
+        }
     },
     {
         path: '/assets',
         name: 'Assets',
-        component: Assets
+        component: Assets,
+        meta: {
+            requires_auth: true
+        }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requires_auth)) {
+        const status_response = await axios.get('https://api.eve-angel.localhost/status', {withCredentials: true})
+
+        if (!status_response.data.is_authenticated) {
+            next({
+                path: '/', // TODO: Maybe redirect to home and remove redirect_uri param instead of redirecting eve login?
+                params: {redirect_uri: to.fullPath}
+            })
+        }
+    } else if (to.matched.some(record => record.meta.requires_guest)) {
+        const status_response = await axios.get('https://api.eve-angel.localhost/status', {withCredentials: true})
+
+        if (status_response.data.is_authenticated) {
+            next({
+                path: '/assets',
+            })
+        }
+    }
+
+    next()
 })
 
 export default router
