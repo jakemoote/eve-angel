bdc64afe4f4a4d0285b2dd3f83ab44be8abed2f1
Jake Moote <jakemoote@gmail.com>
2021-12-31 15:42:23 -0500

    Display assets using ag-grid

--- client/src/components/Assets.vue
+++ client/src/components/Assets.vue
@@ -1,4 +1,8 @@
 <script setup>
+  import { AgGridVue } from "ag-grid-vue3"
+  import "ag-grid-community/dist/styles/ag-grid.css";
+  import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
+
   import axios from "axios";
   import {ref} from "vue";
 
@@ -15,7 +19,7 @@
 
   const getAssets = async () => {
     const assets_response = await axios.get('https://api.eve-angel.localhost/assets', { withCredentials: true })
-    assets.value = assets_response.data
+    assets_row_data.value = assets_response.data
   }
 
   const testSocket = async () => {
@@ -25,7 +29,13 @@
 
   }
 
-  const assets = ref([])
+  const assets_columns = ref([
+    { field: "name", sortable: true, filter: true },
+    { field: "quantity", sortable: true, filter: true },
+    { field: "character.name", sortable: true, filter: true },
+  ])
+
+  const assets_row_data = ref([])
   getAssets()
 </script>
 
@@ -38,22 +48,13 @@
   <button @click="getAssets">Get Assets</button>
   <button @click="testSocket">Test Socket</button>
 
-  <table>
-    <thead>
-    <tr>
-      <th>Name</th>
-      <th>Quantity</th>
-      <th>Character</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="asset in assets">
-      <td>{{ asset.name }}</td>
-      <td>{{ asset.quantity }}</td>
-      <td>{{ asset.character.name }}</td>
-    </tr>
-    </tbody>
-  </table>
+  <AgGridVue
+      style="height: 800px; width: 100%;"
+      class="ag-theme-alpine-dark"
+      :columnDefs="assets_columns"
+      :rowData="assets_row_data"
+  >
+  </AgGridVue>
 </template>
 
 <style scoped>
